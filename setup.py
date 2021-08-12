9b36ef35053c0d220c3ee3c27f01add19de1a57f
Przemek <przemek.lenkiewicz@gmail.com>
2021-08-12 13:35:43 +0200

    a taki tam

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import re
+
+def find_version(filename):
+    _version_re = re.compile(r'__version__ = "(.*)"')
+    for line in open(filename):
+        version_match = _version_re.match(line)
+        if version_match:
+            return version_match.group(1)
+
+__version__ = find_version('rdflib_web/__init__.py')
+
+setup(
+    name = 'rdflib-web',
+    version = __version__,
+    description = "RDFLib Web Apps.",
+    author = "Gunnar Aastrand Grimnes",
+    author_email = "gromgull@gmail.com",
+    url = "https://github.com/RDFLib/rdflib-web",
+    license = "BSD",
+    platforms = ["any"],
+    classifiers = ["Programming Language :: Python",
+                   "License :: OSI Approved :: BSD License",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   "Operating System :: OS Independent",
+                   ],
+    packages = ['rdflib_web'],
+    package_dir = { 'rdflib_web': 'rdflib_web' },
+    package_data = { 'rdflib_web': ['templates/*.html','static/*',]}
+    #packages=find_packages(include=['exampleproject', 'exampleproject.*'])
+)
+
+install_requires = [
+    'flask',
+    'rdflib>=4.0',
+]
+
+
---
+++ test/test_served_sparql_store.py
@@ -0,0 +1,38 @@
+#from nose.exc import SkipTest
+#raise SkipTest("SPARQL store server test skipped")
+import unittest
+import threading
+import rdflib
+import rdflib_web.endpoint
+
+
+class TestSPARQLStore(unittest.TestCase):
+    def testSPARQLStore(self):
+        g=rdflib.Graph(identifier='http://example.org/testgraph')
+
+        data="""<http://example.org/book/book1> <http://purl.org/dc/elements/1.1/title> "SPARQL Tutorial" .
+<http://example.org/book/b\xc3\xb6\xc3\xb6k8> <http://purl.org/dc/elements/1.1/title> "Moose bite can be very n\xc3\xb6sty."@se .
+
+"""
+
+        g.parse(data=data, format='n3')
+
+        # # create our own SPARQL endpoint
+
+        app=rdflib_web.endpoint.get(g)
+        t=threading.Thread(target=lambda : app.run(port=57234))
+        t.daemon=True
+        t.start()
+
+        import time
+        time.sleep(1)
+
+        g2=rdflib.ConjunctiveGraph('SPARQLStore')
+        g2.open("http://localhost:57234/sparql")
+        b=rdflib.URIRef("http://example.org/book/book1")
+        b2=rdflib.URIRef("http://example.org/book/b\xc3\xb6\xc3\xb6k8")
+        DCtitle=rdflib.URIRef("http://purl.org/dc/elements/1.1/title")
+        self.assertEqual(len(list(g2.triples((b,None,None)))), 1)
+        self.assertEqual(list(g2.objects(b,DCtitle))[0], rdflib.Literal("SPARQL Tutorial"))
+
+        self.assertEqual(list(g2.objects(b2,DCtitle))[0], list(g.objects(b2,DCtitle))[0])
